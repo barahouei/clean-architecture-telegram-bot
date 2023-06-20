103b251887979233d3ae9dc63ec547ca40b31a51
barahouei <abdulghanibarahouei@gmail.com>
2023-06-20 07:42:35 +0330

    feat: Add log for debug mode

--- cmd/commands.go
+++ cmd/commands.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/urfave/cli/v2"
 )
@@ -36,7 +37,7 @@ func serve(c *cli.Context) error {
 }
 
 func debug(c *cli.Context) error {
-	fmt.Println("Debug mode is on")
+	log.Println("Debug mode is on")
 	debugMode = true
 
 	return nil
